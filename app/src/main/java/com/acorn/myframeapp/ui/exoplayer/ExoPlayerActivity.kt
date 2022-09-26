1adfcd1c259939cceeee14e0ad223b8111bb64de
oneAcorn <oneacorn@163.com>
2022-09-26 15:20:47 +0800

    exoplayer

--- app/src/main/java/com/acorn/myframeapp/ui/exoplayer/ExoPlayerActivity.kt
+++ app/src/main/java/com/acorn/myframeapp/ui/exoplayer/ExoPlayerActivity.kt
@@ -1,10 +1,14 @@
 package com.acorn.myframeapp.ui.exoplayer
 
 import android.os.Bundle
+import android.os.Looper
+import com.acorn.basemodule.extendfun.logI
 import com.acorn.myframeapp.R
 import com.acorn.myframeapp.base.BaseNoViewModelActivity
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.PlaybackException
+import com.google.android.exoplayer2.Player
 import kotlinx.android.synthetic.main.activity_exo_player.*
 
 /**
@@ -29,7 +33,37 @@ class ExoPlayerActivity : BaseNoViewModelActivity() {
         playerView.player = exoPlayer
         // Set the media item to be played.
         exoPlayer.setMediaItem(MediaItem.fromUri("rtsp://admin:wcy12345@192.168.0.203"))
+        exoPlayer.addListener(object :Player.Listener{
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                super.onPlaybackStateChanged(playbackState)
+                //like Player.STATE_BUFFERING(4种)
+//                Player.STATE_ENDED
+                logI("stateChanged:$playbackState")
+                if(playbackState==Player.STATE_ENDED){
+                    exoPlayer.prepare()
+                }
+            }
+
+            override fun onPlayerError(error: PlaybackException) {
+                super.onPlayerError(error)
+                logI("onPlayerError ${error.message}")
+            }
+        })
+        //Fire Event,特定时间点发送Event
+        exoPlayer.createMessage { messageType, message ->
+            logI("msg:$messageType,$message")
+        }
+            .setLooper(Looper.getMainLooper())
+            .setPosition(5000) //5秒位置发送消息
+            .setPayload("payloads message")
+            .setDeleteAfterDelivery(true)
+            .send()
         // Prepare the player.
         exoPlayer.prepare()
+    }
+
+    override fun onDestroy() {
+        exoPlayer.release()
+        super.onDestroy()
     }
 }
